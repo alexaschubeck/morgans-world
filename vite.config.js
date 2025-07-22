234854a6e387004d11152d5d2ad84fecaad20f4f
alexaschubeck <alexa.schubeck@gmail.com>
2025-07-22 01:16:30 -0500

    Restore base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Temporarily remove base path to test
-  // base: '/morgans-world/',
+  base: '/morgans-world/',
 })
